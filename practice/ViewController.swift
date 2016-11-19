26f069267b5b8c52d5851990c3151260ae2d788e
Chris <cgranthovey@gmail.com>
2016-11-19 15:46:04 -0700

    added love

--- practice/ViewController.swift
+++ practice/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var x = 5
     var y = "second commit"
+    var z = "love in desert"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
